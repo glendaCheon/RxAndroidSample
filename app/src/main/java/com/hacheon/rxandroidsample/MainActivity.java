56ac85840b98034bc3979099e8a77f9430401d80
Glenda Cheon <cheon0915@naver.com>
2019-10-12 14:15:30 +0900

    rxBinding을 이용한 click 이벤트로, 스레드를 옮겨가며 실행

--- app/src/main/java/com/hacheon/rxandroidsample/MainActivity.java
+++ app/src/main/java/com/hacheon/rxandroidsample/MainActivity.java
@@ -3,14 +3,18 @@ package com.hacheon.rxandroidsample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.jakewharton.rxbinding3.view.RxView;
 
 import io.reactivex.Observable;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 import kotlin.Unit;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,12 +26,28 @@ public class MainActivity extends AppCompatActivity {
 
         //rxBinding을 통해 해당 이벤트를 stream화.
         RxView.clicks(findViewById(R.id.button))
+                .subscribeOn(AndroidSchedulers.mainThread())//위치 어딨든 상관없으나, 구동될 때 뭘로 시작할 건질 정하는것임.
+                .doOnNext(new Consumer<Object>() {
+                    @Override
+                    public void accept(Object unit) throws Exception {
+                        Log.d("Main",Thread.currentThread().getName());
+                    }
+                })
+                .observeOn(Schedulers.io())
                 .map(new Function<Object, Object>() {
                     @Override
                     public Object apply(Object o) throws Exception {
                         return "clicked";
                     }
                 })
+                .doOnNext(new Consumer<Object>() {
+                    @Override
+                    public void accept(Object unit) throws Exception {
+                        Log.d("Main",Thread.currentThread().getName());
+                    }
+                })
+//                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())//madin thread로 변경
                 .subscribe(new Consumer<Object>(){
                     @Override
                     public void accept(Object o) throws Exception { //onNext가 된 것.
